a3c1013c0b56039383d75ca8ccdee6c1b31de65d
IvorEda <ivoredafej@gmail.com>
2025-04-02 09:20:09 +0100

    general cleanup

--- src/main/java/com/muzan/musicbookingapp/service/JwtService.java
+++ src/main/java/com/muzan/musicbookingapp/service/JwtService.java
@@ -16,18 +16,17 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
+    // can keep here for now
     private static final String JWT_SECRET = "bddef64c6b57fe4fd348c1f0b9c31397febbaebb1b1e3460bc91ffedf672d5c7d8146a0a49f70dfaba7a9d885668e92e8011459b6a7b29278de5362378c2869faf98153e5e090850eedb598f4912070453d8e32454851f1c7076ac7e1ba29685119de621ae247300844812021ed813a44e727464ebdd5baa6fb1c43dc563b1fe1fe9bea5a56c82acfa262433b07f635b17aa799ace7d8a26cfae127677c9432d5739edd281030a639de43515851accc49df3dda43176485516d227b1cc8de95192675e1281a164fea7a0a3947a1c0c7b9c4f5cf5e2712dfc0e4534fd4e4eef0f6c1b4f2a7d5366f00def9ffc6ede0dbb06db8338b3e16fb588b8f7a0e2df42eb";
 
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
 
-    // Extract username from token
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    // Extract any claim from token
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
@@ -43,23 +42,19 @@ public class JwtService {
                 .compact();
     }
 
-    // Validate token
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
-    // Check if token is expired
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
-    // Extract expiration date
     private Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
-    // Extract all claims from token
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
